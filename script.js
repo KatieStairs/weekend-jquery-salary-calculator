4ac40fe1d930a81c5a97a21a74382dbba13fbad4
Katie Stairs <kstairs831@gmail.com>
2022-12-03 14:26:54 -0600

    Added some HTML changes, worked on the totalMonthlySalary variable, worked on getting the input fields to store in an array. Still need to debug.

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ $(document).ready(readyNow);{
 
 }
 
+let totalMonthlySalary = 20000;
+let employeeInfo = [];
+
 function readyNow(){
-    console.log('in readyNow')
+    //console.log('in readyNow')
     $('body').on('click', '#submitButton', employeeSubmission);
     $('body').on('click', '.delete', deleteEmployee);
-
+    let monthlyPayout = $('#totalMonthlyPayout')
+    monthlyPayout.empty()
+    monthlyPayout.append(totalMonthlySalary.toLocaleString('en-US'))
 }
 
 function deleteEmployee(){
-    console.log('in deleteEmployee')
+    //console.log('in deleteEmployee')
     $(this).parent().parent().remove();
 }
 
 function employeeSubmission(){
-    console.log('in employeeSubmission')
-
+    //console.log('in employeeSubmission')
+    let newEmployeeInfo = {
+        firstName: $('#fNameInput').val(),
+        lastName: $('#lNameInput').val(),
+        id: $('#idInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: $('#annualSalaryInput').val()
+    }
+    employeeInfo.push(newEmployeeInfo);
+    $('#fNameInput').val(''),
+    $('#lNameInput').val(''),
+    $('#idInput').val(''),
+    $('#titleInput').val(''),
+    $('#annualSalaryInput').val('')
 }
+
+
+
+
