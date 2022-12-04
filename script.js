f36f8308109a7716aa4e17313e618fc1c6578f68
Katie Stairs <kstairs831@gmail.com>
2022-12-04 13:40:52 -0600

    Took out hardcoded HTML and put the inputs into a JS function, got rendering  and employee button to work again

--- script.js
+++ script.js
@@ -2,64 +2,121 @@ $(document).ready(readyNow);{
     //console.log('hi')
 }
 
-let totalMonthlySalary = 20000;
+let totalMonthlySalary= 20000;
 let employeeInfo = [];
 
 function readyNow(){
     //console.log('in readyNow')
     $('body').on('click', '#submitButton', employeeSubmissionButton);
-    $('body').on('click', '.deleteEmployeeButton', deleteEmployeeButton);
-    let monthlyPayout = $('#totalMonthlyPayout')
-    monthlyPayout.empty()
-    monthlyPayout.append(totalMonthlySalary.toLocaleString('en-US'))
-
+    $('body').on('click', '#deleteEmployeeButton', deleteEmployeeButton);
+    let monthlyPayoutBudget = $('#remainingMonthlyBudget')
+    monthlyPayoutBudget.empty()
+    monthlyPayoutBudget.append(totalMonthlySalary.toLocaleString('en-US'))
+    renderEmployeeTable()
 }
 
 function deleteEmployeeButton(){
-    //console.log('in deleteEmployee')
+    // console.log('in deleteEmployee')
+    
+    // // let buttonClicked = $(this);
+    // // let tableRow = buttonClicked.parent().parent();
+
+    // // tableRow.remove();
     $(this).parent().parent().remove();
 }
 
 function employeeSubmissionButton(){
     //console.log('in employeeSubmission')
+    let newFName = $('#fNameInput').val();
+    let newLName = $('#lNameInput').val();
+    let newID = $('#idInput').val();
+    let newTitle = $('#titleInput').val();
+    let newAnnualSalary = $('#annualSalaryInput').val()
+
+
     let newEmployeeInfo = {
-        firstName: $('#fNameInput').val(),
-        lastName: $('#lNameInput').val(),
-        id: $('#idInput').val(),
-        title: $('#titleInput').val(),
-        annualSalary: $('#annualSalaryInput').val()
+        firstName: newFName,
+        lastName: newLName,
+        id: Number(newID),
+        title: newTitle,
+        annualSalary: Number(newAnnualSalary)
     }
     employeeInfo.push(newEmployeeInfo);
     console.log('employeeSubmissionButton', employeeInfo)
-    
-    let a = $('#fNameInput').val()
-    let b = $('#lNameInput').val()
-    let c = $('#idInput').val()
-    let d = $('#titleInput').val()
-    let e = $('#annualSalaryInput').val()
-    $('#fNameData').append(a)
-    $('#lNameData').append(b)
-    $('#idData').append(c)
-    $('#titleData').append(d)
-    $('#annualSalaryData').append(e)
 
     $('#fNameInput').val(''),
     $('#lNameInput').val(''),
     $('#idInput').val(''),
     $('#titleInput').val(''),
     $('#annualSalaryInput').val('')
-    calculateMonthlyBalance();
     
+    //calculateMonthlyBalance();
+    //addRow();
+    renderEmployeeTable();
 }
 
-function calculateMonthlyBalance(){
-    console.log('in calculateMonthlyBalance')
+// function calculateMonthlyBalance(){
+//     //subtract salary from employeeInfo from totalMonthlySalary
+//     let monthlySalaryBalance = 0;
+    
+//     for (let i=0; i<employeeInfo.length; i++){
+//         totalMonthlySalary - Number(employeeInfo[i].annualSalary);
+//     }
+//     console.log('in calculateMonthlyBalance')
 
+// }
+
+function renderEmployeeTable(){
+    
+    $('#employeesTable').empty();
+    for (let i = 0; i < employeeInfo.length; i++) {
+    $('#employeesTable').append(`
+        <tr>
+            <td>${employeeInfo[i].firstName}</td>
+            <td>${employeeInfo[i].lastName}</td>
+            <td>${employeeInfo[i].id}</td>
+            <td>${employeeInfo[i].title}</td>
+            <td>${employeeInfo[i].annualSalary}</td>
+            <td> 
+                <button id= deleteEmployeeButton>Delete</button>
+            </td>
+        </tr>
+    `)
+    }
 }
 
-function render(){
-
-}
-
-
-
+    // function calculateMonthlySalary(){
+    //     let employeeMonthlySalaryTemp = 0
+    //      for(let i = 0; i < employeeInfoTable.length; i++){
+    //         employeeMonthlySalaryTemp += Number(employeeInfoTable[i].SalaryList)
+    //      }
+    //       console.log(employeeMonthlySalaryTemp)
+          
+    //       let employeeMonthlySalary = Math.round(employeeMonthlySalaryTemp/12)
+        
+    //       console.log(employeeMonthlySalary)
+        
+          
+    //      let value = ('$' + employeeMonthlySalary);
+    //      $('#totalMonthlySalary').text(`Monthly Total: ${value}`);
+        
+    //      if (employeeMonthlySalary >= 20000){
+    //         $('#TheDiv').addClass('Over20k');
+    //     }
+    //     else{
+    //         $('#TheDiv').removeClass('Over20k');
+    //     }
+    //     console.log(`testing red ${value}`)
+    //      render()
+// } else {
+//     $('#output').empty();
+//     $('#output').append(`
+//         <h2 id="output">
+//             <div id="normal">
+//             Monthly Cost: ${monthlyCosts}
+//             </div>
+//         </h2>
+//     `)
+//     }
+//        }
+//}
